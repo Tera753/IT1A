373aef6742bf919f6e5eae737f9d8e8691b6416c
Tera753 <Tereza.Antosova@student.souepl.cz>
2025-10-13 11:28:33 +0200

    Add files via upload

---
+++ knigtrider_03button.py
@@ -0,0 +1,40 @@
+from machine import Pin
+from utime import sleep
+
+start_pin = 0
+end_pin = 3
+pos = start_pin
+ink = 1
+pins=[]
+
+for i in range(start_pin, end_pin +1):
+    pins.append(Pin(i,Pin.OUT))
+buzzer=Pin(4,Pin.OUT)
+button=Pin(5,Pin.IN,Pin.PULL_DOWN)
+   
+print("Jizda zacina!!!")
+
+while True:
+    try:
+        if button.value():
+            pins[pos].on()
+            sleep(0.2)
+            pins[pos].off()
+            
+            pos += ink
+            
+            if (pos > end_pin) or (pos < start_pin):
+                ink *= -1
+                pos += 2*ink
+                buzzer.on()
+                sleep(0.1)
+                buzzer.off()
+            
+        
+    except KeyboardInterrupt:
+            for pin in pins:
+                pin.off()
+                buzzer.off()
+            print("Konec")
+            break
+        
